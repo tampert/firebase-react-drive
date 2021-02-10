3146c107e83d4d814dc97a3facdc3bc624c9a298
tampert <d_soentjens@hotmail.com>
2021-02-10 15:31:53 +0100

    add navbar

---
+++ src/components/google-drive/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Navbar from "./NavBar";
+
+export default function Dashboard() {
+  return (
+    <>
+      <Navbar />
+      <Container />
+    </>
+  );
+}
